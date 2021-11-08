21e317281b0410578ef609e8d7cfe421abba153d
meriem-djidel <92149090+meriem-djidel@users.noreply.github.com>
2021-11-08 19:46:52 +0100

    Add files via upload

---
+++ jour03/job01/index.php
@@ -0,0 +1,19 @@
+<?php
+$note[0]=200;
+$note[1]=204;
+$note[2]=173;
+$note[3]=98;
+$note[4]=171;
+$note[5]=404;
+$note[6]=459;
+
+if($note [1][5] %2){
+
+echo "$note est pair";
+}
+else{
+    echo"$note est impair";
+}
+
+
+?>
